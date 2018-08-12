64cc233dc8f96fd084ac4826371bdb57062779f1
el_psy_congroo <gaojingjingjing@outlook.com>
2018-08-12 20:20:51 +0900

    'bayes'

--- 4_1/bayes.py
+++ 4_1/bayes.py
@@ -70,26 +70,27 @@ def bagOfWords2VecMN(vocabList,inputSet):
 
 def textParse(bigString):
 	import re
-	listOfTokens=re.split(r'\w*',bigString)
+	listOfTokens=re.split('\w*',bigString)
 	return [tok.lower() for tok in listOfTokens if len(tok)>2]
 
 def spamTest():
 	docList=[]; classList=[]; fullText=[]
 	for i in range(1,26):
-		wordList=textParse(open('email/spam/%d.txt'%i).read())
+		#print(i)
+		wordList=textParse(open('email/spam/%d.txt'%i,encoding='utf-8').read())
 		docList.append(wordList)
 		fullText.extend(wordList)
 		classList.append(1)
-		wordList=textParse(open('email/ham/%d.txt'%i).read())
+		wordList=textParse(open('email/ham/%d.txt'%i,encoding='utf-8').read())
 		docList.append(wordList)
 		fullText.extend(wordList)
 		classList.append(0)
 	vocabList=createVocabList(docList)
-	trainingSet=range(50); testSet=[]
+	trainingSet=list(range(50)); testSet=[]
 	for i in range(10):
-		randIndex=int(rndom.uniform(0,len(trainingSet)))
+		randIndex=int(random.uniform(0,len(trainingSet)))
 		testSet.append(trainingSet[randIndex])
-		del(trainingSet)
+		del(trainingSet[randIndex])
 	trainMat=[]; trainClasses=[]
 	for docIndex in trainingSet:
 		trainMat.append(setOfWords2Vec(vocabList,docList[docIndex]))
@@ -101,6 +102,68 @@ def spamTest():
 		if classifyNB(array(wordVector),p0V,p1V,pSpam)!=classList[docIndex]:
 			errorCount+=1
 	print('the error rate is: ',float(errorCount)/len(testSet))
+
+def calcMostFreq(vocabList,fullText):
+	import operator
+	freqDict={}
+	for token in vocabList:
+		freqDict[token]=fullText.count(token)
+	sortedFreq=sorted(freqDict.items(),key=operator.itemgetter(1),reverse=True)
+	#print(sortedFreq[0][0])
+	return sortedFreq[:30]
+
+def localWords(feed1,feed0):
+	import feedparser
+	docList=[]; classList=[]; fullText=[]
+	minLen=min(len(feed1['entries']),len(feed0['entries']))
+	for i in range(minLen):
+		wordList=textParse(feed1['entries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		wordList=textParse(feed0['entries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	vocabList=createVocabList(docList)
+	top30Words=calcMostFreq(vocabList,fullText)
+	#print(top30Words)
+	for partW in top30Words:
+		#print(partW)
+		if partW[0] in vocabList: vocabList.remove(partW[0])
+	trainingSet=list(range(2*minLen)); testSet=[]
+	for i in range(20):
+		randIndex=int(random.uniform(0,len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat=[]; trainClasses=[]
+	for docIndex in trainingSet:
+		trainMat.append(bagOfWords2VecMN(vocabList,docList[docIndex]))
+		trainClasses.append(classList[docIndex])
+	p0V,p1V,pSpam=trainNB0(array(trainMat),array(trainClasses))
+	errorCount=0
+	for docIndex in testSet:
+		wordVector=bagOfWords2VecMN(vocabList,docList[docIndex])
+		if classifyNB(array(wordVector),p0V,p1V,pSpam)!=classList[docIndex]:
+			errorCount+=1
+	print('the error rate is: ',float(errorCount)/len(testSet))
+	return vocabList,p0V,p1V
+
+def getTopWords(ny,sf):
+	import operator
+	vocabList,p0V,p1V=localWords(ny,sf)
+	topNY=[]; topSF=[]
+	for i in range(len(p0V)):
+		if p0V[i]>-4.0: topSF.append((vocabList[i],p0V[i]))
+		if p1V[i]>-1.2: topNY.append((vocabList[i],p1V[i]))
+	sortedSF=sorted(topSF,key=lambda pair:pair[1],reverse=True)
+	print('SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF')
+	for item in sortedSF:
+		print(item[0])
+	sortedNY=sorted(topNY,key=lambda pair:pair[1],reverse=True)
+	print('NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY')
+	for item in sortedNY:
+		print(item[0])
 
 if __name__=='__main__':
 	listOPosts,listClasses=loadDataSet()
@@ -116,4 +179,12 @@ if __name__=='__main__':
 	p0V,p1V,pAb=trainNB0(trainMat,listClasses)
 	print(pAb)
 	print(p0V,'\n',p1V)
+	#print(sum(p0V),sum(p1V))
 	testingNB()
+	spamTest()
+	import feedparser
+	ny=feedparser.parse('http://www.nasa.gov/rss/dyn/image_of_the_day.rss')
+	sf=feedparser.parse('http://www.cppblog.com/kevinlynx/category/6337.html/rss')
+	vocabList,pSF,pNY=localWords(ny,sf)
+	vocabList,pSF,pNY=localWords(ny,sf)
+	getTopWords(ny,sf)
